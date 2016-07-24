0c93df6a92fd639a2a4ea09de47380cde9eb3168
Tevin Jeffrey <tev.jeffrey@gmail.com>
2016-07-23 22:28:49 -0400

    Removed dependency

--- Pods/Target Support Files/JSONCodable/JSONCodable-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double JSONCodableVersionNumber;
-FOUNDATION_EXPORT const unsigned char JSONCodableVersionString[];
-
--- Pods/Target Support Files/Pods-UniversityCourseTracker/Pods-UniversityCourseTracker-frameworks.sh
+++ Pods/Target Support Files/Pods-UniversityCourseTracker/Pods-UniversityCourseTracker-frameworks.sh
@@ -85,11 +85,9 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/JSONCodable/JSONCodable.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ProtocolBuffers-Swift/ProtocolBuffers.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/JSONCodable/JSONCodable.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ProtocolBuffers-Swift/ProtocolBuffers.framework"
 fi
